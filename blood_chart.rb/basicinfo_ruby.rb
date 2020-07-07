452cd5ba8196166e1e8c2b07e86eff17d51e1bbd
sjones4215 <bluesfansince2013@gmail.com>
2020-07-06 22:16:31 -0500

    Initial commit

---
+++ blood_chart.rb/basicinfo_ruby.rb
@@ -0,0 +1,14 @@
+puts "Hello enter your height"
+height = gets.chomp
+puts "Enter your weight"
+weight = gets.chomp
+puts "Enter recent blood pressure"
+bp = gets.chomp
+puts "Enter your sex"
+sex = gets.chomp
+puts "Enter a recent heart rate"
+hr = gets.chomp
+puts "Enter a recent temperature"
+temp = gets.chomp
+
+puts "#{height}, #{weight}, #{bp}, #{sex}, #{hr}, #{temp}"
